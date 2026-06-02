ff62369f475ba3a1abb95918951f5be27b0849c8
Priyanshu61103 <priyanshuparashar108@gmail.com>
2026-06-02 21:57:19 +0530

    commit change 31

--- react/src/Components/Navbar.jsx
+++ react/src/Components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
     <div className={mode==="light"?"h-25 w-full flex items-center justify-around border-b-4 bg-white border-orange-600":"h-25 w-full flex items-center justify-around border-b-4 bg-black border-orange-600"}>
       <div>
         <Link to="/">
-          <div className="flex items-center md:mr-5">
+          <div className="flex items-center">
             <img
               src="../coinlogo.png"
               alt=""
@@ -40,46 +40,8 @@ const Navbar = () => {
         </Link>
       </div>
 
-      {/* <div className="w-100 flex justify-around text-xl font-bold">
-        <div
-          className={
-            mode === "light"
-              ? "text-black hidden md:block"
-              : "text-white hidden md:block"
-          }
-        >
-          Home
-        </div>
-        <div
-          className={
-            mode === "light"
-              ? "text-black hidden md:block"
-              : "text-white hidden md:block"
-          }
-        >
-          Features
-        </div>
-        <div
-          className={
-            mode === "light"
-              ? "text-black hidden md:block"
-              : "text-white hidden md:block"
-          }
-        >
-          Pricing
-        </div>
-        <div
-          className={
-            mode === "light"
-              ? "text-black hidden md:block"
-              : "text-white hidden md:block"
-          }
-        >
-          Blog
-        </div>
-      </div> */}
 
-      <div className="h-15 w-60 flex justify-center items-center mr-2 md:mr-0">
+      <div className="h-15 w-60">
         <select
           name=""
           id="countryDropdown"
@@ -105,8 +67,8 @@ const Navbar = () => {
           <button
             className={
               mode === "light"
-                ? "h-10 w-10 rounded-full border-2 border-black bg-black  flex justify-center items-center ml-5 mr-5"
-                : "h-10 w-10 rounded-full border-2 border-white bg-white flex justify-center items-center ml-5 mr-5"
+                ? "h-10 w-10 rounded-full border-2 border-black bg-black  flex justify-center items-center"
+                : "h-10 w-10 rounded-full border-2 border-white bg-white flex justify-center items-center"
             }
             onClick={() => dispatch(setMode())}
           >
