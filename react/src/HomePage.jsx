175b01a4e8697af3b4145b9cb286d747bdcc2d83
Priyanshu61103 <priyanshuparashar108@gmail.com>
2026-06-02 18:22:43 +0530

    commit changes 7

--- react/src/HomePage.jsx
+++ react/src/HomePage.jsx
@@ -32,9 +32,8 @@ const HomePage = () => {
       },
     };
     console.log(currencySign);
-    console.log(currency.payload);
     let data;
-    if (currency.payload == undefined) {
+    if (!currency.payload) {
       data = await fetch(
         `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&names=${name}&order=market_cap_desc&per_page=10`,
         options
